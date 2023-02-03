a24e724986f4461c5cb45ac4b5d961e71289bb51
Tom Phillips <tomphillipsmusic@gmail.com>
2023-02-03 13:37:53 -0500

    Initial Commit

---
+++ MathMountain/MathMountainApp.swift
@@ -0,0 +1,17 @@
+//
+//  MathMountainApp.swift
+//  MathMountain
+//
+//  Created by Tom Phillips on 2/3/23.
+//
+
+import SwiftUI
+
+@main
+struct MathMountainApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
